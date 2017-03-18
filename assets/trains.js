0bd89d507d3862a5cf1ecc6c4a77316fe3df81af
Hoang Desktop <hxuanpham@gmail.com>
2017-03-18 09:09:18 -0700

    all i got for now

--- assets/trains.js
+++ assets/trains.js
@@ -1,32 +1,31 @@
-// algorithm(){
-// 	var baseline;
-// 	var freq;
-// 	var now = moment();
-// 	var runningTime = moment(baseline).toNow();
-// 	var timeToNextTrain = runningTime%freq;
-// 	var trains = nextTrain/freq;
-// 	var nextTrain = trains++;
-// 	nextTrain = nextTrain * freq;
-// 	moment(baseline).add(nextTrain, "minutes");
-// }
+//keeping track of the trains
+var trainIndex;
+var database = firebase.database();
+database.ref("index").on("value", function(snapshot) {
+  trainIndex = snapshot.val();
+});
+
+//event listener - click event
+$("#addNew").on("click", function(){
+	//update index
+	database.ref("index").set(trainIndex+1);
+	//pull user inputs
+	var newTrain = {
+	name : $("#name").val(),
+	dest : $("#dest").val(),
+	first : $("#first").val(),
+	freq : $("#freq").val()
+	};
+	// //add to database
+	database.ref("trains").push().set(newTrain);
+});
 
 
+//event listener - firebase
+database.ref("trains").on("value", function(snapshot) {
+  //stuff
+});
 
-
-
-
-//event listener
-	//on click
-		//update firebase
-	//display new train
-
-//update from firebase
-	//pull from firebase
-	//clear DOM
-	//display all trains
-
-//display
-	//convert firebase elements into DOM elements
 
 //init
 	//update from firebase
@@ -37,3 +36,15 @@
 //algorithm
 	//take vars
 	//return next train & next arrival
+
+
+// 	first;
+// 	freq;
+// 	var now = moment();
+// 	var runningTime = moment(first).toNow();
+// 	var timeToNextTrain = runningTime%freq;
+// 	var trains = nextTrain/freq;
+// 	var nextTrain = trains++;
+// 	nextTrain = nextTrain * freq;
+// 	moment(baseline).add(nextTrain, "minutes");
+// }
