b0a2e8fd38b8f20b2a5a92761a67f181093717a3
Hoang Desktop <hxuanpham@gmail.com>
2017-03-18 01:46:00 -0700

    inital commit

---
+++ assets/trains.js
@@ -0,0 +1,39 @@
+// algorithm(){
+// 	var baseline;
+// 	var freq;
+// 	var now = moment();
+// 	var runningTime = moment(baseline).toNow();
+// 	var timeToNextTrain = runningTime%freq;
+// 	var trains = nextTrain/freq;
+// 	var nextTrain = trains++;
+// 	nextTrain = nextTrain * freq;
+// 	moment(baseline).add(nextTrain, "minutes");
+// }
+
+
+
+
+
+
+//event listener
+	//on click
+		//update firebase
+	//display new train
+
+//update from firebase
+	//pull from firebase
+	//clear DOM
+	//display all trains
+
+//display
+	//convert firebase elements into DOM elements
+
+//init
+	//update from firebase
+
+//timer
+	//update from firebase
+
+//algorithm
+	//take vars
+	//return next train & next arrival
